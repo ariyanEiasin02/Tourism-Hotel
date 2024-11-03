9ab82e78ed6f4ca20f52521f67c67c240a9fb0ad
Md Eiasin <ariyaneiasin02@gmail.com>
2024-11-03 22:36:23 +0600

    code update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      keyframes: {
-        backgroundImages: {
-          '0%': { backgroundImage: 'url("./src/assets/popular1.jpg")' },
-          '20%': { backgroundImage: 'url("./src/assets/popular2.jpg")' },
-          '40%': { backgroundImage: 'url("./src/assets/popular3.jpg")' },
-          '60%': { backgroundImage: 'url("./src/assets/popular4.jpg")' },
-          '80%': { backgroundImage: 'url("./src/assets/popular5.jpg")' },
-          '100%': { backgroundImage: 'url("./src/assets/popular6.jpg")' },
-        },
-      },
-      animation: {
-        backgroundImages: 'backgroundImages 30s infinite ease-in-out', // Adjust duration as needed
-      },
       maxWidth:{
         "container" : "1200px"
       },
