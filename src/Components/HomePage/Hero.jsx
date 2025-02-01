ae2fc6e7c98b0789d23bb3ab75247c32b6478371
aklimaakter20 <aklimaakter20@gmail.com>
2025-02-01 13:39:35 +0600

    testing running

---
+++ src/Components/HomePage/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className='pb-16 bg-red-500'>
+        <div className="container mx-auto">
+            <h1>Hello Home Page !</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolorem atque nesciunt explicabo consequuntur dignissimos aspernatur quasi. Exercitationem laboriosam atque voluptatem in enim corporis molestias deleniti illum ratione quod debitis eos, veritatis odit. Aspernatur laudantium hic exercitationem et quidem! Quam, laudantium eveniet voluptatibus aliquid autem adipisci praesentium iste! Placeat, libero.</p>
+        </div>
+      
+    </section>
+  )
+}
+
+export default Hero
