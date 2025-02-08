5f72af0d966b5c25415067ed3b74d6ce59c859c3
Md Eiasin <ariyaneiasin02@gmail.com>
2025-02-08 22:15:03 +0600

    Revert "Merge pull request #1 from habibulla552272/main"
    This reverts commit 531732c7e6e4918e40209880ae1cdf33c7500b90, reversing
    changes made to beb7f360986d1ffb4c1dcd99b14ff0124045f34f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
-import "slick-carousel/slick/slick.css";
+import { useState } from 'react'
+import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Footer from "./Components/Footer/Footer";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import BookNow from "./Pages/BookNow/BookNow";
-import BackToTop from "./Components/BackToTop/BackToTop";
-import Navbar from "./Components/Navbar/Navbar";
-import Header from "./Components/Header/Header";
-import TourDetails from "./Components/ExploreHotes/Tourse1/TourDetails";
+import BackToTop from './Components/BackToTop/BackToTop';
+import Navbar from './Components/Navbar/Navbar';
+import Header from './Components/Header/Header';
 function App() {
+ 
   return (
     <>
       <Router>
-        <BackToTop />
-        <Header />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/booknow" element={<BookNow />} />
-          <Route path="/TourDetails" element={<TourDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
+        <BackToTop/>
+        <Header/>
+        <Navbar/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booknow" element={<BookNow />} />
+      </Routes>
+      <Footer/>
+    </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
