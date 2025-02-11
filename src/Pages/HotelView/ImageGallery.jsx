4d0481c5d88e9d4b68989b67461aa45be609d4ea
aklimaakter20 <aklimaakter20@gmail.com>
2025-02-11 14:23:18 +0600

    Related-Hotel section responsive done

--- src/Pages/HotelView/ImageGallery.jsx
+++ src/Pages/HotelView/ImageGallery.jsx
@@ -3,37 +3,38 @@ import Slider from "react-slick";
 
 function ImageGallery() {
   const settings = {
+    
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 2,
-   
     
   };
   return (
     
-    <div className="px-4 mt-10 mb-10  slider-container md:px-8 lg:px-10">
-         <img className='mb-5 rounded-lg' src="hotel-big-1.png" alt="" />
+    <div className="w-full px-4 mb-20 slider-container md:px-8 lg:px-16">
+      <img className="mb-5" src="hotel-big-1.png" alt="" />
       <Slider {...settings}>
-        
-        <div className="w-5 h-auto mr-3">
-          <img className="pr-3" src="small-1.png" alt="" />
+        {/* main-image */}
+        <div className="w-10 h-auto ">
+          <img src="small-1.png" alt="" />
         </div>
-        <div className="w-5 h-auto">
-        <img className="pr-3" src="small-2.png" alt="" />
+        {/* slider-image */}
+        <div className="w-5 h-auto md:w-8 ">
+        <img className="mr-4 md:mr-6" src="small-2.png" alt="" />
         </div>
-        <div className="w-5 h-auto">
-        <img className="pr-3" src="small-3.png" alt="" />
+        <div className="w-5 h-auto md:w-8 ">
+        <img className="mr-4 md:mr-6" src="small-3.png" alt="" />
         </div>
-        <div className="w-5 h-auto">
-        <img className="pr-3" src="small-4.png" alt="" />
+        <div className="w-5 h-auto md:w-8 ">
+        <img className="mr-4 md:mr-6" src="small-4.png" alt="" />
         </div>
-        <div className="w-5 h-auto">
-        <img className="pr-3" src="small-5.png" alt="" />
+        <div className="w-5 h-auto md:w-8 ">
+        <img className="mr-4 md:mr-6" src="small-5.png" alt="" />
         </div>
-        <div className="w-5 h-auto">
-        <img className="pr-3" src="small-6.png" alt="" />
+        <div className="w-5 h-auto md:w-8 ">
+        <img className="mr-4 md:mr-6" src="small-6.png" alt="" />
         </div>
        
     
--- src/Pages/HotelView/RelatedHotelAll.jsx
+++ src/Pages/HotelView/RelatedHotelAll.jsx
@@ -6,11 +6,31 @@ import Slider from "react-slick";
 function RelatedHotelAll() {
   
     const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 4,
-        slidesToScroll: 3
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true
+            
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+      
+      ]
       };
   return (
     <section>
